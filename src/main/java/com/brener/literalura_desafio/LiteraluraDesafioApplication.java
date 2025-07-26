d509b99f72255ec16fb3941f23356e37dbcb29ee
Brener Oliveira Brito <=>
2025-07-26 15:55:23 -0300

    Primeiro Commit

---
+++ src/main/java/com/brener/literalura_desafio/LiteraluraDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.brener.literalura_desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraDesafioApplication.class, args);
+	}
+
+}
